2bc17f264e4857839fbf75d871e6ebef52892769
daniykt <daniykt7@gmail.com>
2026-06-27 16:28:07 -0300

    limpando a estrutura do projeto.

--- src/components/perfil/index.js
+++
@@ -1,4 +0,0 @@
-export { default as CabecalhoPerfil } from "./CabecalhoPerfil";
-export { default as EstatisticasPerfil } from "./EstatisticasPerfil";
-export { default as AbasPerfil } from "./AbasPerfil";
-export { default as GaleriaPerfil } from "./GaleriaPerfil";
