ed1cf73716c5c212f2bc9a2433dede0262c50820
daniykt <daniykt7@gmail.com>
2026-06-30 11:48:32 -0300

    feat(auth): implementa AuthRepository HTTP integrado com API real

---
+++ pesque_fale_app/lib/features/auth/data/auth_repository.dart
@@ -0,0 +1,12 @@
+import '../domain/auth_result.dart';
+
+abstract class AuthRepository {
+  Future<AuthResult> cadastrar({
+    required String nome,
+    required String email,
+    required String senha,
+    required String confirmarSenha,
+  });
+
+  Future<AuthResult> login({required String email, required String senha});
+}
