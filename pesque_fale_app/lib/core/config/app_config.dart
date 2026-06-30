2e761e29fae73cf141f9635d65d830687f97b8df
daniykt <daniykt7@gmail.com>
2026-06-30 11:45:33 -0300

    feat(core): adiciona AppConfig com API_BASE_URL e USE_MOCK configuraveis

---
+++ pesque_fale_app/lib/core/config/app_config.dart
@@ -0,0 +1,10 @@
+class AppConfig {
+  AppConfig._();
+
+  static const String apiBaseUrl = String.fromEnvironment(
+    'API_BASE_URL',
+    defaultValue: 'http://10.0.2.2:3333/v1',
+  );
+
+  static const bool useMock = bool.fromEnvironment('USE_MOCK', defaultValue: false);
+}
